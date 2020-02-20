61c82e1b3523571a2a98191b4182950bf626471d
Wendy Figueroa <wendy.e.figueroa@outlook.com>
2020-02-20 08:59:49 -0500

    first push

--- Program.cs
+++ Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+using System.Net.Mime;
+using System.Threading;
+
 
 namespace Star_Wars_Trading_Game
 {
     class Program
     {
+       
+        
+
         static void Main(string[] args)
         {
-            
+           new Main_Menu().Run();
+           //new Planet().Run();
         }
+
+
+
+       
+      
     }
+
+    
 }
